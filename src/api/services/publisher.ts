8edf45061a7ce43aa9eff650fafd84e1ff5687bf
Rennan Ribas <rennanrr@hotmail.com>
2021-04-18 19:01:06 -0300

    [websocket] fix on websocket connection

--- src/api/services/publisher.ts
+++ src/api/services/publisher.ts
@@ -3,7 +3,7 @@ import amqp from 'amqplib/callback_api';
 const sendMessage = (msg: string) =>
   amqp.connect('amqp://localhost:5672', (err, connection) => {
     connection.createChannel((err, channel) => {
-      var q = 'hello';
+      var q = 'Galley';
       channel.assertQueue(q, { durable: false });
       channel.sendToQueue(q, Buffer.from(msg));
       console.log(" [x] Sent %s", msg);
--- src/api/services/worker.ts
+++ src/api/services/worker.ts
@@ -1,20 +1,22 @@
 import amqp from 'amqplib/callback_api';
-import { socketIO } from '../../app';
-const worker = (msg: string) =>
+import { Server } from 'socket.io';
+const worker = (socketIO: Server) => {
   amqp.connect('amqp://localhost:5672', (err, connection) => {
     connection.createChannel((err, channel) => {
-      var q = 'hello';
+      var q = 'Galley';
 
-        channel.assertQueue(q, { durable: false });
-        channel.prefetch(1);
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-        channel.consume(q, function (msg) {
-          socketIO.on("connection", socket => {
-            socket.send(msg?.content.toString());
-          });
-          console.log(" [x] Received %s", msg?.content.toString());
-        }, { noAck: true });
+      channel.assertQueue(q, { durable: false });
+      channel.prefetch(1);
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+      channel.consume(q, function (msg) {
+        if (msg) {
+          const message: { string1: string, string2: string } = JSON.parse(msg.content.toString());
+          socketIO.send(message.string1 + message.string2);
+          console.log(`[${q}] Received %s ${msg.content.toString()}`);
+        }
+      }, { noAck: true });
     });
   });
+}
 
-  export default worker
+export default worker
